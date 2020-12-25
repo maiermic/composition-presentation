8a68a61eddcac44f66fcadf220a33d1e110834cb
Michael Maier <maier1michael@gmail.com>
2020-12-25 11:42:46 +0100

    cleanup and improve documentation of implementation and examples

--- examples/functional/functional-promise-example.ts
+++ examples/functional/functional-promise-example.ts
@@ -1,92 +1,138 @@
 import { pipe } from './pipe'
 
+// The term Monad is used in comments of this file as defined in Haskell, i.e.
+//
+//     class Monad m where
+//       (>>=)  :: m a -> (  a -> m b) -> m b
+//       (>>)   :: m a ->  m b         -> m b
+//       return ::   a                 -> m a
+//       fail   :: String -> m a
+//
+//       m >> k =  m >>= \_ -> k
+//
+// See https://wiki.haskell.org/Monad#Monad_class
+
+// ----------------------------------------------------------
+// Functional Promise (called Task to avoid naming conflicts)
+// ----------------------------------------------------------
+
 type Task<R, E> = (callbacks: {
+  /** Monad `return` definition */
   resolve: (result: R) => void
+  /** Monad `fail` definition */
   reject: (error: E) => void
 }) => any
 
+// Example that creates a Task
 function sleep(seconds): Task<number, never> {
   return ({ resolve, reject }) =>
     setTimeout(() => resolve(seconds), seconds * 1000)
 }
 
-// Monad `bind` definition
+// Monad `bind` definition of Task with
+//   * `resolve` as Monad `return` definition
+//   * `reject` as Monad `fail` definition
+//
+// ---\--- return --[ bind(x => fn(x)) ]--\-- resolve --->
+//     \-- fail ---------------------------\--- reject --->
 const bindTask = f => task => ({ resolve, reject }) =>
   task({
     resolve: result => f(result)({ resolve, reject }),
     reject,
   })
 
+// Not part of the Monad definition or rather another `bind` implementation
+// that is the inverted version of the regular `bind` definition above with
+//   * `reject` as Monad `return` definition
+//   * `resolve` as Monad `fail` definition
+//
+// ---\--- return -----------------------------/-- resolve -->
+//     \-- fail --[ catchError(e => fn(e)) ]--/--- reject --->
 const catchError = f => task => ({ resolve, reject }) =>
   task({
     resolve,
     reject: error => f(error)({ resolve, reject }),
   })
 
-// bindTask(x => x + 1)(sleep(1))({
-//   resolve: x => console.log(`task finished with result ${x}`),
-//   reject: e => console.log(`task finished with error ${e}`),
-// })
-
-// bindTask(x => ({ resolve, reject }) => resolve(x + 10))(sleep(1))({
-//   resolve: x => console.log(`task finished with result ${x}`),
-//   reject: e => console.log(`task finished with error ${e}`),
-// })
-
-// bindTask(x => ({ resolve, reject }) => reject(x + ' is invalid'))(sleep(1))({
-//   resolve: x => console.log(`task finished with result ${x}`),
-//   reject: e => console.log(`task finished with error ${e}`),
-// })
-
-pipe(
+const taskExamplePipe = pipe(
   bindTask(x => ({ resolve, reject }) => resolve(x + 10)),
   bindTask(x => ({ resolve, reject }) => reject('some error')),
   bindTask(x => ({ resolve, reject }) => resolve(x * 2)),
   catchError(error => ({ resolve, reject }) => resolve(0)),
   bindTask(x => ({ resolve, reject }) => resolve(x + 100)),
-)(sleep(1))({
+)
+
+taskExamplePipe(sleep(1))({
   resolve: x => console.log(`task finished with result ${x}`),
   reject: e => console.log(`task finished with error ${e}`),
 })
 
-// Monad `return` definition (success case)
+// Helper similar to `Promise.resolve`
 const resolveTask = x => ({ resolve, reject }) => resolve(x)
-// Monad `fail` definition
+// Helper similar to `Promise.reject`
 const rejectTask = e => ({ resolve, reject }) => reject(e)
 
-pipe(
+// Example with helpers
+const taskExample2Pipe = pipe(
   bindTask(x => resolveTask(x + 10)),
   bindTask(x => rejectTask('some error')),
   bindTask(x => resolveTask(x * 2)),
   catchError(error => resolveTask(0)),
   bindTask(x => resolveTask(x + 100)),
-)(sleep(1))({
+)
+
+taskExample2Pipe(sleep(1))({
   resolve: x => console.log(`task finished with result ${x}`),
   reject: e => console.log(`task finished with error ${e}`),
 })
 
-// function bind<R, E>(next: (result: R, error: E) => any) {
-//   return function bound<E>(task: Task<R, E>) {
-//     return (value: R) => task(next, reject)
-//   }
-// }
+// ---------------
+// Regular Promise
+// ---------------
 
+// Monad `bind` definition of Promise with
+//   * `Promise.resolve` as Monad `return` definition
+//   * `Promise.reject` as Monad `fail` definition
+//
+// ---\--- return --[ bind(x => fn(x)) ]--\-- resolve --->
+//     \-- fail ---------------------------\--- reject --->
 const bind = f => p => p.then(f)
 
-// pipe(
-//   bind(x => x + 1),
-//   bind(x => x * 2),
-// )((resolve, reject) => resolve(4))
+// Not part of the Monad definition or rather another `bind` implementation
+// that is the inverted version of the regular `bind` definition above with
+//   * `Promise.reject` as Monad `return` definition
+//   * `Promise.resolve` as Monad `fail` definition
+//
+// ---\--- return ------------------------/-- resolve -->
+//     \-- fail --[ catch(e => fn(e)) ]--/--- reject --->
+const catchPromiseError = f => p => p.catch(f)
 
-const result: Promise<any> = pipe(
-  bind(x => x + 1),
+const promiseExamplePipe = pipe(
+  // Function passed to bind (then-method) of Promise is aware of Monad context,
+  // i.e. returns a Promise
+  bind(x => Promise.resolve(x + 1)),
+  // but bind is overloaded and calls resolve internally if passed function is
+  // not aware of Monad context, i.e. returns a non-Promise value that is passed
+  // internally to resolve.
   bind(x => x * 2),
-  // )(Promise.resolve(4))
-)(Promise.reject(4))
-
-result.then(
-  x => console.log(`Promise finished with result: ${x}`),
-  e => console.error(`Promise finished with result: ${e}`),
+  // If function is aware of Monad context, it may fail (reject).
+  // Otherwise, it can not fail.
+  bind(x =>
+    x >= 0 ? x : Promise.reject(`expected positive number, but got ${x}`),
+  ),
+  // No helpers used
+  bind(x => new Promise((resolve, reject) => resolve(x + 1))),
+  bind(x => console.log(`Promise finished with result: ${x}`)),
+  catchPromiseError(e => console.error(`Promise finished with error: ${e}`)),
 )
 
-// const bindOptional = <T>(f: (value: T) => any) => (value: T) =>
+// Examples
+
+promiseExamplePipe(Promise.resolve(4))
+// Promise finished with result: 10
+
+promiseExamplePipe(Promise.resolve(-4))
+// Promise finished with error: expected positive number, but got -6
+
+promiseExamplePipe(Promise.reject('initial error'))
+// Promise finished with error: initial error
