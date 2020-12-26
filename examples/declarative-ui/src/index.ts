d2fca35ec3fa52136e0e9cb01c529db8e63abb72
Michael Maier <maier1michael@gmail.com>
2020-12-26 16:35:20 +0100

    add count to app

--- examples/declarative-ui/src/index.ts
+++ examples/declarative-ui/src/index.ts
@@ -1,18 +1,23 @@
 import { createElement, mount, render } from './vdom'
 
-const vApp = createElement('div', {
-  attrs: {
-    id: 'app',
-  },
-  children: [
-    'Hello world', // represents TextNode
-    createElement('img', {
-      attrs: {
-        src: 'https://media.giphy.com/media/cuPm4p4pClZVC/giphy.gif',
-      },
-    }), // represents ElementNode
-  ],
-}) // represents ElementNode
+const createVApp = (count: number) =>
+  createElement('div', {
+    attrs: {
+      id: 'app',
+      dataCount: String(count), // we use the count here
+    },
+    children: [
+      'The current count is: ',
+      String(count), // and here
+      createElement('img', {
+        attrs: {
+          src: 'https://media.giphy.com/media/cuPm4p4pClZVC/giphy.gif',
+        },
+      }),
+    ],
+  })
 
-const $app: HTMLElement = render(vApp)
+let count = 0
+const vApp = createVApp(count)
+const $app = render(vApp)
 mount($app, document.getElementById('app'))
