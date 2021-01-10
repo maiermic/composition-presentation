baa68c4c592442b345a67c18e6f7c7d728eb4d66
Michael Maier <maier1michael@gmail.com>
2021-01-10 17:54:20 +0100

    add more helper tags: a, footer, h1, header, label, section, span

--- examples/declarative-ui/src/vdom-helpers.ts
+++ examples/declarative-ui/src/vdom-helpers.ts
@@ -7,10 +7,17 @@ export function createElementHelper(tagName: string) {
   ) => createElement(tagName, { attrs, children })
 }
 
+export const a = createElementHelper('a')
 export const button = createElementHelper('button')
 export const div = createElementHelper('div')
+export const footer = createElementHelper('footer')
 export const form = createElementHelper('form')
+export const h1 = createElementHelper('h1')
+export const header = createElementHelper('header')
 export const img = createElementHelper('img')
 export const input = createElementHelper('input')
+export const label = createElementHelper('label')
 export const li = createElementHelper('li')
 export const ul = createElementHelper('ul')
+export const section = createElementHelper('section')
+export const span = createElementHelper('span')
