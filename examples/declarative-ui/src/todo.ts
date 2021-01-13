cdf859af23473f5981094048d696c356f7d4ef10
Michael Maier <maier1michael@gmail.com>
2021-01-13 12:41:00 +0100

    implement Todo-app (declarative-ui example) according to the TodoMVC project
    See http://todomvc.com/

--- examples/declarative-ui/src/todo.ts
+++ examples/declarative-ui/src/todo.ts
@@ -1,21 +1,54 @@
 import { diff, mount, render } from './vdom'
-import { button, form, input, li, ul } from './vdom-helpers'
+import {
+  a,
+  button,
+  div,
+  footer,
+  h1,
+  header,
+  input,
+  label,
+  li,
+  section,
+  span,
+  ul,
+} from './vdom-helpers'
 
 interface Todo {
   text: string
+  completed: boolean
 }
 
 class State {
   text: string = ''
   todos: Todo[] = []
+  leftTodosCount = 0
 
   addTodo = (): void => {
-    this.todos.push({ text: this.text })
+    this.todos.push({ text: this.text, completed: false })
+    this.leftTodosCount++
     rerender()
   }
 
   removeTodo = (index: number): void => {
-    this.todos.splice(index, 1)
+    const [deletedTodo] = this.todos.splice(index, 1)
+    if (!deletedTodo.completed) {
+      this.leftTodosCount--
+    }
+    rerender()
+  }
+
+  toggleTodo = (index: number): void => {
+    const todo = this.todos[index]
+    todo.completed = !todo.completed
+    this.leftTodosCount += todo.completed ? -1 : 1
+    rerender()
+  }
+
+  clearCompletedTodos() {
+    console.log('clearCompletedTodos')
+    this.todos = this.todos.filter(todo => !todo.completed)
+    this.leftTodosCount = this.todos.length
     rerender()
   }
 
@@ -25,43 +58,78 @@ class State {
   }
 }
 
-const createVApp = ({ text, addTodo, removeTodo, setText, todos }: State) =>
-  form(
-    {
-      onSubmit(_event: Event) {
-        console.log('submit')
-        addTodo()
-      },
-      action: 'javascript:',
-    },
-    [
+const createVApp = (state: State) =>
+  section({ class: 'todoapp' }, [
+    header({ class: 'header' }, [
+      h1('todos'),
       input({
-        value: text,
-        onInput(event: Event) {
+        class: 'new-todo',
+        placeholder: 'What needs to be done?',
+        value: state.text,
+        autofocus: '',
+        onInput(event: InputEvent) {
           const inputText = (event.target as HTMLInputElement).value
-          console.log('input', inputText)
-          setText(inputText)
+          state.setText(inputText)
+        },
+        onKeyup(event: KeyboardEvent) {
+          if (event.key === 'Enter') {
+            state.addTodo()
+            state.setText('')
+          }
         },
       }),
-      button({ type: 'submit' }, ['Add']),
+    ]),
+    section({ class: 'main' }, [
+      input({ id: 'toggle-all', class: 'toggle-all', type: 'checkbox' }),
+      label({ for: 'toggle-all' }, 'Mark all as complete'),
       ul(
-        {},
-        todos.map(({ text }: Todo, index) =>
-          li({}, [
-            text,
-            input({
-              type: 'button',
-              value: 'X',
-              onClick(_event: Event) {
-                console.debug('remove TODO', index)
-                removeTodo(index)
-              },
-            }),
+        { class: 'todo-list' },
+        state.todos.map((todo: Todo, index) =>
+          li({ class: todo.completed ? 'completed' : '' }, [
+            div({ class: 'view' }, [
+              input({
+                class: 'toggle',
+                type: 'checkbox',
+                checked: todo.completed,
+                onClick(_event: Event) {
+                  state.toggleTodo(index)
+                },
+              }),
+              label(todo.text),
+              button({
+                class: 'destroy',
+                onClick(_event: Event) {
+                  console.debug('remove TODO', index)
+                  state.removeTodo(index)
+                },
+              }),
+            ]),
+            input({ class: 'edit', value: 'Rule the web' }),
           ]),
         ),
       ),
-    ],
-  )
+      footer({ class: 'footer' }, [
+        span({ class: 'todo-count' }, [
+          String(state.leftTodosCount),
+          ' item left',
+        ]),
+        ul({ class: 'filters' }, [
+          li([a({ href: '#/', class: 'selected' }, 'All')]),
+          li([a({ href: '#/active' }, 'Active')]),
+          li([a({ href: '#/completed' }, 'Completed')]),
+        ]),
+        button(
+          {
+            class: 'clear-completed',
+            onClick(_event: Event) {
+              state.clearCompletedTodos()
+            },
+          },
+          'Clear completed',
+        ),
+      ]),
+    ]),
+  ])
 
 const state: State = new State()
 let vApp = createVApp(state)
